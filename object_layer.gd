403f44003a7daa06d5d5bdbca2b1435af376d8f3
Mackenzie Beck <mack.beck977@gmail.com>
2025-09-30 17:37:58 -0600

    Created tilemaplayers for game environment figured out basics of how rocket and goal will be displayed

---
+++ object_layer.gd
@@ -0,0 +1,15 @@
+extends TileMapLayer
+
+
+@export var player_sprite_atlas_coords : Vector2 = Vector2(0,10)
+@export var goal_sprite_atlas_coords : Vector2 = Vector2(0,6)
+
+@export var player_coords : Vector2 = Vector2(0,0)
+@export var goal_coords : Vector2 = Vector2(10,-10)
+
+
+func _ready() -> void:
+	set_cell(player_coords, 1, player_sprite_atlas_coords)
+	set_cell(goal_coords,1,  goal_sprite_atlas_coords)
+	print(get_cell_tile_data(Vector2(0,0)))
+	
