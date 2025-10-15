836a9eb0c9e9c3d0b9fd14ae4f789785be945b46
Mackenzie Beck <mack.beck977@gmail.com>
2025-10-14 21:10:09 -0600

    terms in term set are now shuffled

--- UI/term_set.gd
+++ UI/term_set.gd
@@ -5,7 +5,10 @@ const TERM = preload("uid://c3lam4sdf8qcb")
 @onready var grid_container: GridContainer = $MarginContainer/GridContainer
 
 func update_term_set(tokens: Array, num: int) -> void:
-	for token in tokens:
+	var shuffled_tokens = tokens.duplicate()
+	shuffled_tokens.shuffle()
+	
+	for token in shuffled_tokens:
 		var new_term = TERM.instantiate()
 		new_term.set_term_text(token)
 		grid_container.add_child(new_term)
