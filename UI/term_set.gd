be9bf8d9df4e8881d153b304a3f1142644c4e2bf
Mackenzie Beck <mack.beck977@gmail.com>
2025-10-14 21:13:07 -0600

    term set now has extra terms

--- UI/term_set.gd
+++ UI/term_set.gd
@@ -4,6 +4,13 @@ extends PanelContainer
 const TERM = preload("uid://c3lam4sdf8qcb")
 @onready var grid_container: GridContainer = $MarginContainer/GridContainer
 
+var term_set = [
+	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+	"+", "-", "*", "=",
+]
+
+
+
 func update_term_set(tokens: Array, num: int) -> void:
 	var shuffled_tokens = tokens.duplicate()
 	shuffled_tokens.shuffle()
@@ -11,4 +18,9 @@ func update_term_set(tokens: Array, num: int) -> void:
 	for token in shuffled_tokens:
 		var new_term = TERM.instantiate()
 		new_term.set_term_text(token)
+		grid_container.add_child(new_term)
+		
+	for temp in range(0,tokens.size()):
+		var new_term = TERM.instantiate()
+		new_term.set_term_text(term_set.pick_random())
 		grid_container.add_child(new_term)
