0d3cb38f0ed569ff51792ac6a729a32605c0b4c5
Mackenzie Beck <mack.beck977@gmail.com>
2025-10-15 20:23:01 -0600

    grabbed term updates with click and follows the mouse

--- UI/term_interface.gd
+++ UI/term_interface.gd
@@ -4,15 +4,24 @@ extends Control
 #@onready var grabbed_term: Term = $GrabbedTerm
 @onready var equation_template: EquationTemplate = $EquationTemplate
 @onready var term_set: PanelContainer = $TermSet
+@onready var grabbed_term: Term = $GrabbedTerm
 
 
 
 
+func _physics_process(delta: float) -> void:
+	if grabbed_term.visible:
+		grabbed_term.global_position = get_global_mouse_position() + Vector2(5,5)
 
 
-
-func _on_term_clicked(button: int) -> void:
-	print(button)
+func _on_term_clicked(button: int, term_text) -> void:
+	match button:
+		1:
+			grabbed_term.set_term_text(term_text)
+			grabbed_term.visible = !grabbed_term.visible
+			print(term_text)
+		2:
+			print("right click")
 	
 	
 func set_equation_template(num : int) -> void:
