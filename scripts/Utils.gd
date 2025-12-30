3affd8cea980119ea5ee905962ca573877f793c7
Mackenzie(work) <mack.beck977@gmail.com>
2025-12-30 11:39:43 -0700

    game plays a random track on repeat

--- scripts/Utils.gd
+++ scripts/Utils.gd
@@ -7,3 +7,10 @@ var points : int = 0
 var rng = RandomNumberGenerator.new()
 
 signal update_line(equation : String)
+
+
+
+func play_music():
+	print("play music on game.gd")
+	var tmp = rng.randi_range(26,34)
+	AudioManager.create_audio(tmp)
